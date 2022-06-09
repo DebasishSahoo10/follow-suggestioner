f738cdb787d9a5ace695fa779bd5cbed63213436
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-06-09 19:21:26 +0530

    feat : added more entries and added desc Added more entries in wedding section and added descriptions to every entry

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ import "./styles.css";
 
 var accountsDB = {
   Wedding: [
-    { name: "House on the clouds", rating: "4.5/5" },
-    { name: "Sanvera : the wedding reels", rating: "4/5" }
+    { name: "House on the clouds", rating: "5/5", description: "Anything" },
+    {
+      name: "Sanvera : the wedding reels",
+      rating: "4.5/5",
+      description: "Anything"
+    },
+    { name: "Dot Dusk", rating: "4/5", description: "Anything" },
+    { name: "What Knot In", rating: "3/5", description: "Anything" },
+    {
+      name: "Cine Love Productions",
+      rating: "3/5",
+      description:
+        "A team of enthusiastic storytelling wedding photographers who know nothing more than passionately crafting memories of your wedding day that you’d want to revisit, into love stories you’d want to cherish for a lifetime."
+    }
   ],
   Street: [
     { name: "Moises Levy", rating: "4.5/5" },
@@ -44,6 +56,7 @@ export default function App() {
             <li className="list">
               <div> {account.name} </div>
               <div> {account.rating} </div>
+              <div> {account.description} </div>
             </li>
           ))}
         </ul>
